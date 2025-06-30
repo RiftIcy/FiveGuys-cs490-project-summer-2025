af2d07d068aaeca6fce0e965955d34397cd32839
Lior <ultron895@gmail.com>
2025-06-30 02:50:28 -0400

    Fixed timezone issues.

--- back_end/db.py
+++ back_end/db.py
@@ -6,7 +6,7 @@ load_dotenv(dotenv_path=os.path.join(os.path.dirname(os.path.dirname(__file__)),
 
 MONGO_URI = os.getenv("MONGO_URI")
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(MONGO_URI, tz_aware=True)
 db = client['fiveguys']
 biography_collection = db['biographies']
 job_ads_collection = db['job ads']
